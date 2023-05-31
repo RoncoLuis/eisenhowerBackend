2ac026e47ec9b23b6f4997a64fd9ee8c8f6cdbb8
luise <w580ix@gmail.com>
2023-05-31 08:18:34 -0600

    2nd commint Validate if task description is an empty string add a comment -- No comments added --

--- src/main/java/com/eisenhower/matrix/controller/TaskController.java
+++ src/main/java/com/eisenhower/matrix/controller/TaskController.java
@@ -4,13 +4,11 @@ package com.eisenhower.matrix.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.eisenhower.matrix.model.Task;
 import com.eisenhower.matrix.services.ITaskService;
@@ -27,7 +25,7 @@ public class TaskController {
 	@PostMapping("/save")
 	public String newTask(@ModelAttribute Task theNewtask) {
 		theNewtask.setIdTask(autoIncrementTaskId());
-		if(theNewtask.getTaskDescription() == ""){
+		if(theNewtask.getTaskDescription().equals("")){
 			theNewtask.setTaskDescription("-- No comments added --");
 		}
 		taskService.createTask(theNewtask);
